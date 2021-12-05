ee35b84a1c735320e1f4f5e320059c5da5daa15c
Ramilkaa <94863485+Ramilkaa@users.noreply.github.com>
2021-12-05 23:06:14 +0300

    Update dz1.py

--- dz1.py
+++ dz1.py
@@ -7,9 +7,9 @@ def f(x):
     return 100 * (np.sqrt(abs(1 - 0.01 * x ** 2))) + 0.01 *(abs(x + 10))
 x_min = -15
 x_max = 5
-dx = 0.1
+dx = 0.05
 
-x = np.arange(x_min, x_max, dx)
+x = np.arange(x_min, x_max + 0.05, dx)
 y = f(x)
 
 res = {
@@ -20,7 +20,11 @@ res = {
 path = pathlib.Path("results")
 path.mkdir(exist_ok=True)
 file = path / "result_task1.json"
+out = file.open("w")
+json.dump(res, out, indent=4)
+out.close()
 
 pylab.plot(x, y)
 pylab.grid()
+pylab.show()
 pylab.savefig("results/task1.png")
